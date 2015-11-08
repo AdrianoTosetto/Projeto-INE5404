644a2e7602a8976f36ea777c5eeb7ac3d08d5f36
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-11-08 15:12:43 -0200

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,5 +1,7 @@
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -7,17 +9,37 @@ import javax.swing.JPanel;
 
 public class Level1 extends JPanel{
 
+	public Level1(Player p, ArrayList<Enemy> e){
+		player = p;
+		enemies = e;
+	}
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public static String BACKGROUND_IMAGE_PATH;
-	
-	
+	public static String BACKGROUND_IMAGE_PATH = "res//background.png";
+	private Player player;
+	 ArrayList<Enemy> enemies;
+	 
+	 
 	public void paint(Graphics g){
 		Graphics2D gd2 = (Graphics2D)g;
-		new Player(Game.x,10).draw(gd2);
-		new Enemy(100,100).draw(gd2);
+		gd2.drawImage(new ImageIcon(BACKGROUND_IMAGE_PATH).getImage(),0,0,null);
+		
+		player.draw(gd2);
+		player.drawBullets(gd2);
+		
+		for(int i = 0; i < enemies.size();i++){
+			enemies.get(i).draw(gd2);
+		}
+		Game.hb.draw(g);
+		
 		g.dispose();
+	}
+	
+	void update(Player p, ArrayList<Enemy> e){
+		player = p;
+		enemies = e;
 	}
 }
