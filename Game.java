ad8c48c74775e60f85c70acc0642d0b018d214ee
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-11-08 15:07:34 -0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,32 +1,55 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Game extends JFrame{
+public class Game extends JFrame implements KeyListener{
 
-	
-	private Player player;
-	private Level1 level;
-	
 	private ArrayList<Enemy> gameEnemies = new ArrayList<Enemy>();
+	static Player player = new Player(10,10);
+	private Level1 level = new Level1(player,gameEnemies);
+	static HealthBar hb = new HealthBar(1, 420);
 	
 	private Timer gameTimer;
 	
 	public static int DEFAULT_FRAME = 100;
 	static int x = 100;
 	
+	public static void main(String[] args){
+	
+		
+		new Game(new Player(10,10),new Level1(null,null));
+	}
+	
+	
+	
 	public Game(Player p,Level1 l){
-		level = l;
-		player = p;
 		setSize(500,500);
 		add(level);
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {}
+			@Override
+			public void keyReleased(KeyEvent e) {
+				player.shoot(e);
+			}
+			@Override
+			public void keyPressed(KeyEvent e) {
+				player.move(e);
+			}
+		});
 		
 		 gameTimer = new Timer();
 		 gameTimer.scheduleAtFixedRate(new TimerTask() {
@@ -35,16 +58,45 @@ public class Game extends JFrame{
 			public void run() {
 				level.revalidate();
 				level.repaint();
-				System.out.println("hue");
-				Game.x++;
+				generateEnemies();
+				updateMissiles();
+				updateEnemies();
+				//hb.update(hb.getW() - 1);
+				
 			}
-		},1, 1000);
+		},5, 10);
 
 	}
 	
-	public static void main(String[] args){
-	
+	private void updateMissiles(){
 		
-		new Game(new Player(10,10),new Level1());
+		ArrayList<Bullet> bullets = player.getMissiles();
+		
+		for(int i = 0; i < bullets.size();i++){
+			if(!bullets.get(i).isVisible()){
+				player.bullets.remove(i);
+			}
+		}
 	}
+	private void updateEnemies(){
+		for(int i = 0; i < gameEnemies.size();i++){
+			if(!gameEnemies.get(i).isVisible()){
+				gameEnemies.remove(i);
+			}
+		}
+	}
+	private void generateEnemies(){
+		if(gameEnemies.size() < 20){
+			gameEnemies.add(new Enemy((new Random().nextInt(500)) + 500,new Random().nextInt(500)));
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+	@Override
+	public void keyPressed(KeyEvent e) {}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
 }
