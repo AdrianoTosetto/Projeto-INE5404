0817929709feb1171ad1a6d161493768a280031c
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-12-02 17:26:48 -0200

    Create LitteAsteroid.java

---
+++ LitteAsteroid.java
@@ -0,0 +1,10 @@
+
+public class LitteAsteroid extends Enemy{
+
+	public static String URL_IMAGE_PATH_LITTLE_ASTEROID = "res//small_asteroid1.gif";
+	
+	public LitteAsteroid(int x, int y, int s) {
+		super(x, y, s,URL_IMAGE_PATH_LITTLE_ASTEROID);
+	}
+
+}
