a46d78a23373d38f8461a1ad742c5de5f70a92de
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-12-02 17:27:11 -0200

    Create BigAsteroid.java

---
+++ BigAsteroid.java
@@ -0,0 +1,10 @@
+
+public class BigAsteroid extends Enemy{
+	
+	public static String URL_IMAGE_PATH_BIG_ASTEROID = "res//small_asteroid.gif";
+	
+	public BigAsteroid(int x, int y, int s) {
+		super(x, y, s,URL_IMAGE_PATH_BIG_ASTEROID);
+	}
+
+}
